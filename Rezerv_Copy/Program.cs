901d948ea1df1d57b20def7d3a66096689367433
dimonk1ss <dklrrb@gmail.com>
2022-10-21 22:11:03 +0300

    Rezerv_copy

---
+++ Rezerv_Copy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+namespace Rezerv_Copy
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"rcconfig1.json");
+            var config = configurationBuilder.Build();
+            var copyConfig = config
+                .GetSection("Copy_Path");
+            var c1 = new Copy_Path()
+            {
+                source_path = copyConfig.GetSection("source_path").Value,
+                target_path = copyConfig.GetSection("target_path").Value
+            };
+            c1.Copy(c1.source_path, c1.target_path);
+
+            
+            
+        }
+    }
+}
---
+++ Rezerv_Copy_Library/Copy_Path.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Rezerv_Copy
+{
+    public class Copy_Path 
+    {
+        public string source_path { get; set; }
+        public string target_path { get; set; }
+        
+        public Copy_Path()
+        {
+
+        }
+        public void Copy(string source_path, string target_path)
+        {
+
+
+            foreach (var directory in Directory.GetDirectories(source_path))
+            {
+                string dirName = Path.GetFileName(directory);
+                if (!Directory.Exists(Path.Combine(target_path, dirName)))
+                {
+                    Directory.CreateDirectory(Path.Combine(target_path, dirName));
+                }
+                Copy(directory, Path.Combine(target_path, dirName));
+            }
+
+            foreach (var file in Directory.GetFiles(source_path))
+            {
+                File.Copy(file, Path.Combine(target_path, Path.GetFileName(file)));
+            }
+        }
+
+
+    }
+}
